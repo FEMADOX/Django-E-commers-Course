110975b9436055178f4fd331013378552c0ca968
Fenyxz <enmadofenyxz@gmail.com>
2025-01-14 01:13:29 +0000

    Refactor URL patterns and update template links for account and cart functionality

--- account/urls.py
+++ account/urls.py
@@ -7,7 +7,7 @@ app_name = "account"
 urlpatterns = [
     path("", views.user_account, name="user_account"),
     path("update/", views.update_account, name="update_account"),
-    re_path(r".*/login-signup/$", views.create_user, name="create_user"),
+    re_path(r".*login-signup/$", views.create_user, name="create_user"),
     path("login/", views.login_user, name="login_user"),
     path("logout/", views.logout_user, name="logout_user"),
 
