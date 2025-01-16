749cbd5278c8014cb1a07b12c9dfdc2c9d3c269e
Fenyxz <enmadofenyxz@gmail.com>
2025-01-16 17:54:51 +0000

    Payment in process

--- order/views.py
+++ order/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 
 from account.forms import ClientForm
 from account.models import Client
+from order.models import Order
 
 # Create your views here.
 
@@ -45,4 +46,32 @@ def create_order(request: HttpRequest):
 
 @login_required(login_url="login/")
 def confirm_order(request: HttpRequest):
-    return render(request, "order.html")
+    user = User.objects.get(pk=request.user.pk)
+
+    if request.method == "POST":
+        user.first_name = request.POST["name"]
+        user.last_name = request.POST["last_name"]
+        user.email = request.POST["email"]
+        user.save()
+    try:
+        client = Client.objects.get(user=user)
+        client.phone = request.POST["phone"]
+        client.address = request.POST["address"]
+        client.save()
+    except Client.DoesNotExist:
+        client = Client.objects.create(
+            user=user,
+            address=request.POST["address"],
+            phone=request.POST["phone"],
+        )
+    new_order = Order()
+    new_order.client = client
+    new_order.save()
+    order_num = f"Order #{new_order.pk} - "\
+        f"Date {new_order.registration_date.strftime('%Y')}"
+    total_price = request.session["cart_total_price"]
+    new_order.order_num = order_num
+    new_order.total_price = total_price
+    new_order.save()
+
+    return render(request, "shipping.html", {"order": new_order})
