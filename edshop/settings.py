f38c294af55720433c0c71258ea4d2bbdd2cc7c7
Giancarlos <enmadofenyxz@gmail.com>
2025-04-17 23:53:27 -0400

    Update settings.py

--- edshop/settings.py
+++ edshop/settings.py
@@ -150,12 +150,13 @@ STORAGES = {
     },
 }
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
