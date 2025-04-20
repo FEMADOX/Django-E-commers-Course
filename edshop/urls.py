3c74a5b056ff733a1df37b80f9ae59ca35e5f745
Giancarlos <enmadofenyxz@gmail.com>
2025-04-20 17:22:48 -0400

    Update urls.py

--- edshop/urls.py
+++ edshop/urls.py
@@ -31,11 +31,11 @@ urlpatterns = [
     re_path(r".*login/$", login_user, name="login_user"),
 ]
 
-urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT,
-)
-urlpatterns += static(
-        settings.STATIC_URL,
-        document_root=settings.STATIC_ROOT,
-)
+# urlpatterns += static(
+#         settings.MEDIA_URL,
+#         document_root=settings.MEDIA_ROOT,
+# )
+# urlpatterns += static(
+#         settings.STATIC_URL,
+#         document_root=settings.STATIC_ROOT,
+# )
