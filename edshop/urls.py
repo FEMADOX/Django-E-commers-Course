8aa510db52ceb12aa825437d5d69b56c423f59cb
Giancarlos <enmadofenyxz@gmail.com>
2025-04-19 20:20:53 -0400

    Update urls.py

--- edshop/urls.py
+++ edshop/urls.py
@@ -37,5 +37,5 @@ urlpatterns += static(
 )
 urlpatterns += static(
         settings.STATIC_URL,
-        document_root=settings.STATICFILES_DIR,
+        document_root=settings.STATICFILES_DIRS,
 )
